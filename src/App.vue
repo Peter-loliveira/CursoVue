5ea71e47caf9fa83b8f41533a5f3a9abe7081aed
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2023-01-31 13:21:54 -0300

    Diretiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,206 @@
 <template>
-  <TheHeader v-if = 'showHeader'/>
+  <TheHeader v-if="showHeader" />
 
-  <div v-show = 'showName'>
-    Nome: {{ firstName }} <br>
+  <div v-show="showName">
+    Nome: {{ firstName }} <br />
     Sobrenome: {{ lastName }}
   </div>
 
-  <div v-if = "accessLevel === 'admin'">Admionistrador</div>
-  <div v-else-if="accessLevel === 'marketing'" >Equipe de Marketing</div>
+  <div v-if="accessLevel === 'admin'">Admionistrador</div>
+  <div v-else-if="accessLevel === 'marketing'">Equipe de Marketing</div>
   <div v-else>User</div>
 
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Bem vindo ao seu APP em Vue.js App"/>
+  <br />
+  <div>
+    <select>
+      <Option id="firstItem">Selecione uma CICOM</Option>
+      <Option v-for="cicom in cicoms" v-bind:key="cicom.index">{{cicom.cicom}} - {{ cicom.circuito }}</Option>
+    </select>
+  </div>
+
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Bem vindo ao seu APP em Vue.js App" /> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheHeader from './components/TheHeader.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import TheHeader from "./components/TheHeader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
     // eslint-disable-next-line vue/no-unused-components
-    TheHeader
+    TheHeader,
   },
-  data(){
+  data() {
     return {
       showHeader: true,
-      firstName: 'Jon',
-      lastName: 'Snow',
+      firstName: "Jon",
+      lastName: "Snow",
       showName: true,
-      accessLevel: 'marketing'
-    }
-  }
-}
+      accessLevel: "marketing",
+      cicoms: [
+        {
+          cicom: "Alagoinhas",
+          circuito: "ALGRG0028I",
+          RG: "4",
+          Tel: "(75)3423-9200",
+        },
+        {
+          cicom: "Barreiras",
+          circuito: "BES5031525",
+          RG: "3",
+          Tel: "(77)3611-0190",
+        },
+        {
+          cicom: "Barreiras",
+          circuito: "BRSRG0027I",
+          RG: "4",
+          Tel: "(77)3611-0190",
+        },
+        {
+          cicom: "Brumado",
+          circuito: "BMDRG0010I",
+          RG: "4",
+          Tel: "(77)3453-2649",
+        },
+        {
+          cicom: "Esplanada",
+          circuito: "ESPRG0006I",
+          RG: "4",
+          Tel: "(77)3413-7600",
+        },
+        {
+          cicom: "Euclides da Cunha",
+          circuito: "ECNRG0010I",
+          RG: "4",
+          Tel: "(75)32710190",
+        },
+        {
+          cicom: "Feira de Santana",
+          circuito: "FRSRG0074I",
+          RG: "4",
+          Tel: "(75)3224-0918",
+        },
+        {
+          cicom: "Guanambi",
+          circuito: "GNB5017824",
+          RG: "3",
+          Tel: "(77)3451-5399",
+        },
+        {
+          cicom: "Guanambi",
+          circuito: "GMBRG0014I",
+          RG: "4",
+          Tel: "(77)3451-5399",
+        },
+        {
+          cicom: "Ibotirama",
+          circuito: "ITMRG0006I",
+          RG: "4",
+          Tel: "(77)3698-6500",
+        },
+        {
+          cicom: "Irecê",
+          circuito: "IRCRG0019I",
+          RG: "4",
+          Tel: "(74)3641-5935",
+        },
+        {
+          cicom: "Itaberaba",
+          circuito: "ITRRG0018I",
+          RG: "4",
+          Tel: "(75)3251-8450",
+        },
+        {
+          cicom: "Itabuna",
+          circuito: "ITBRG0036I",
+          RG: "4",
+          Tel: "(73)32141500",
+        },
+        {
+          cicom: "Jequié",
+          circuito: "JQERG0035I",
+          RG: "4",
+          Tel: "(73)3528-3900",
+        },
+        {
+          cicom: "Juazeiro",
+          circuito: "JUARG0041I",
+          RG: "4",
+          Tel: "(74)3611-9230",
+        },
+        {
+          cicom: "Paulo Afonso",
+          circuito: "PAFRG0023I",
+          RG: "4",
+          Tel: "(75)3281-8900",
+        },
+        {
+          cicom: "Porto Seguro",
+          circuito: "PGU5022195",
+          RG: "3",
+          Tel: "(73)3268-9330",
+        },
+        {
+          cicom: "Porto Seguro",
+          circuito: "PSGRG0020I",
+          RG: "4",
+          Tel: "(73)3268-9330",
+        },
+        {
+          cicom: "Santa Maria da Vitoria",
+          circuito: "SMVRG0014I",
+          RG: "4",
+          Tel: "(77)3483-6549",
+        },
+        {
+          cicom: "Santo Antônio de Jesus",
+          circuito: "SAJRG0016I",
+          RG: "4",
+          Tel: "(75)3631-8450",
+        },
+        {
+          cicom: "Senhor do Bonfim",
+          circuito: "SDBRG0018I",
+          RG: "4",
+          Tel: "(74)3541-3995",
+        },
+        {
+          cicom: "Serrinha",
+          circuito: "SRRRG0019I",
+          RG: "4",
+          Tel: "(75) 3261-8247",
+        },
+        {
+          cicom: "Teixeira de Freitas",
+          circuito: "TXFRG0020I",
+          RG: "4",
+          Tel: "(73)3165-2199",
+        },
+        {
+          cicom: "Valença",
+          circuito: "VEC5015775",
+          RG: "3",
+          Tel: "(75)36433400",
+        },
+        {
+          cicom: "Valença",
+          circuito: "VLCRG0018I",
+          RG: "4",
+          Tel: "(75)36433400",
+        },
+        {
+          cicom: "Vitoria da Conquista",
+          circuito: "VCARG0038I",
+          RG: "4",
+          Tel: "(77) 3420-8800",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
@@ -46,5 +212,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 </style>
