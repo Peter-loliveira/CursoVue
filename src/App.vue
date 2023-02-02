ca7143eeca5da178d417ef28fd82bfab238c0db8
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2023-02-02 16:18:37 -0300

    v-bind class

--- src/App.vue
+++ src/App.vue
@@ -30,18 +30,25 @@
       <Option v-for="cicom  in cicoms" v-bind:key="cicom.index">{{cicom.cicom}} - {{ cicom.circuito }}</Option>
     </select>
   </div>
-  
   <hr>
+  
   <div>
     <h1>
       v-bind
     </h1>
     <div>
       <img :src="imgSrc" :alt="imgAlt" class="smallImg">
-      
     </div>
   </div>
-  
+  <hr>
+
+  <div>
+    <button @click="changeClass">Mudar Classe</button>
+      <h1 :class="{'title': true, 'title-home': isHome}" >Classe dinâmica</h1>
+
+      <p :class="['title', {'title-home': isHome}]">Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Suco de cevadiss deixa as pessoas mais interessantis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+      Mé faiz elementum girarzis, nisi eros vermeio. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum.</p>
+    </div>
   <hr>
 
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
@@ -68,6 +75,7 @@ export default {
       accessLevel: "marketing",
       imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Doctor_Who_Logo_1973-1980.svg',
       imgAlt:'Foto do Jon Snow',
+      isHome: false,
       cicoms: [
         {
           cicom: "Alagoinhas",
@@ -228,7 +236,13 @@ export default {
       ],
     };
   },
+  methods: {
+    changeClass: function () {
+      this.isHome = !this.isHome
+    }
+  }
 };
+
 </script>
 
 <style>
@@ -243,5 +257,29 @@ export default {
 
 .smallImg{
   width: 10rem ;
+}
+
+.title {
+  color: blue;
+  font-size: 20px;
+}
+.title-home{
+  color: green;
+  font-size: 40px;
+}
+
+button{
+  font-size: 15px;
+  font-weight: bold;
+  color: #fff;
+  background-color: red;
+  border-color: #fff;
+  padding: 5px 10px;
+  border-radius: 20px;
+  width: 150px;
+  height: 40px;
+}
+button:hover{
+  background-color: black;
 }
 </style>
